716f8eb0f3c482ae986f0598178fb50ba0f7fbd6
RaulRivera <rirpgamer.12@hotmail.com>
2023-12-06 13:41:15 -0800

    milllion channge auto

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
-import million from "million/compiler";
-
+import million from 'million/compiler';
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
-
-export default million.next(nextConfig);
+ 
+const millionConfig = {
+  auto: true,
+  // if you're using RSC:
+  // auto: { rsc: true },
+}
+ 
+export default million.next(nextConfig, millionConfig);
